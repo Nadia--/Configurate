bbbb1141904a5b375ae87a6546836abf0f42c5f4
Nadia-- <figs.in.your.face@gmail.com>
2016-06-23 22:38:22 -0500

    added the folders for vim

---
+++ .vim
@@ -0,0 +1,1 @@
+/home/osipova2/Documents/dotfiles/vimfiles
--- setup.sh
+++ setup.sh
@@ -1,14 +1,16 @@
 mv ~/.bashrc ~/.bashrc.bak
 mv ~/.bash_profile ~/.bash_profile.bak
+mv ~/.vimrc ~/.vimrc.bak
+mv ~/.vim ~/.vim.bak
 mv ~/.gitconfig ~/.gitconfig.bak
 mv ~/.tmux.conf ~/.tmux.conf.bak
-mv ~/.vimrc ~/.vimrc.bak
 
 ln -s `pwd`/.bashrc ~/.bashrc
 ln -s `pwd`/.bash_profile ~/.bash_profile
+ln -s `pwd`/.vimrc ~/.vimrc
+ln -s `pwd`/.vim ~/.vim
 ln -s `pwd`/.gitconfig ~/.gitconfig
 ln -s `pwd`/.tmux.conf ~/.tmux.conf
-ln -s `pwd`/.vimrc ~/.vimrc
 
 #cp .bashrc ~/.bashrc
 #cp .bash_profile ~/.bash_profile
